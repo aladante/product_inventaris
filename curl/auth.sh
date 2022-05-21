a80cc7827927282ee34bdc5634db26e286e22316
yung whatever <janjr@base.nl>
2022-05-21 17:23:42 +0200

    scritps

--- curl/auth.sh
+++ curl/auth.sh
@@ -1,3 +1,3 @@
 curl -X POST localhost:8080/api/auth/signin \
 	-H "Content-Type: application/json" \
-	-d '{"username": "adminn", "password": "adminn", "role":"admin"}'
+	-d '{"username": "adminn", "password": "adminn"}'
