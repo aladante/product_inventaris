90b560b0c1c5f3124dd9c4057034542685a154e4
yung whatever <janjr@base.nl>
2022-06-14 09:28:35 +0200

    correct curl

--- curl/signup.sh
+++ curl/signup.sh
@@ -2,5 +2,5 @@ curl -X POST 'http://34.111.112.34/graphql'  \
 	-H 'Accept: application/json, multipart/mixed' \
 	-H 'Accept-Language: en-US,en;q=0.5'  \
 	-H 'content-type: application/json'\
-	--data-raw '{"query":"mutation {\n  createUser(input: {username: \"addminn\", password: \"secret\" role: [\"ADMIN\"]}) {\n    id\n    username\n  }\n}","operationName":null}'
+	--data-raw '{"query":"mutation {\n  createUser(input: {username: \"admin\", password: \"secret\" role: [\"admin\"]}) {\n    id\n    username\n  }\n}","operationName":null}'
 
